4e8469b630c4405c91fd41a5c26d07e58448e9bf
Paolo Drago Leon s296101 <100195874+paolodragol@users.noreply.github.com>
2024-12-14 19:15:33 +0100

    feat: add initial express server setup

--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,34 @@
-const http = require('http');
+const express = require('express');
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-type': 'application/json',
-  });
-  res.end(
-    JSON.stringify({
-      name: 'Isaac Newton',
-    })
-  );
+const app = express();
+
+const friends = [
+  {
+    id: 0,
+    name: 'Albert Einstein',
+  },
+  {
+    id: 1,
+    name: 'Sir Isaac Netwon',
+  },
+];
+
+app.get('/friends', (req, res) => {
+  res.json(friends);
 });
 
-server.listen(PORT, () => {
+app.get('/friends/:friendId', (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if (friend) res.json(friend);
+  else
+    res.status(400).json({
+      error: `Friend ${friendId} does not exist`,
+    });
+});
+
+app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}...`);
 });
